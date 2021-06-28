dfd87a57f3d9e7775c5ba7ef96217fd1415c9501
Atilla <63123956+atillakalay@users.noreply.github.com>
2021-06-28 20:19:14 +0300

    Ajax post operations have been prepared.

---
+++ ProgremmesBlog.Mvc/Areas/Admin/Models/CategoryAddAjaxViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Entities.Dtos;
+
+namespace ProgrammersBlog.Mvc.Areas.Admin.Models
+{
+    public class CategoryAddAjaxViewModel
+    {
+        public CategoryAddDto CategoryAddDto { get; set; }
+        public string CategoryAddPartial { get; set; }
+        public CategoryDto CategoryDto { get; set; }    
+    }
+}
