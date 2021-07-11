1af0335f4e0533327d63fb7fc74376dabc7d9446
Atilla <63123956+atillakalay@users.noreply.github.com>
2021-07-11 16:24:00 +0300

    The UserPasswordChangeDto Class has been created.

---
+++ Entities/Dtos/UserPasswordChangeDto.cs
@@ -0,0 +1,31 @@
+﻿
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Entities.Dtos
+{
+    public class UserPasswordChangeDto
+    {
+        [DisplayName("Şu Anki Şifreniz")]
+        [Required(ErrorMessage = "{0} boş geçilmemelidir.")]
+        [MaxLength(30, ErrorMessage = "{0} {1} karekterden büyük olmamalıdır.")]
+        [MinLength(10, ErrorMessage = "{0} {1} karekterden küçük olmamalıdır.")]
+        [DataType(DataType.Password)]
+        public string CurrentPassword { get; set; }
+
+        [DisplayName("Yeni Şifreniz")]
+        [Required(ErrorMessage = "{0} boş geçilmemelidir.")]
+        [MaxLength(30, ErrorMessage = "{0} {1} karekterden büyük olmamalıdır.")]
+        [MinLength(10, ErrorMessage = "{0} {1} karekterden küçük olmamalıdır.")]
+        [DataType(DataType.Password)]
+        public string NewPassword { get; set; }
+
+        [DisplayName("Yeni Şifrenizin Tekrarı")]
+        [Required(ErrorMessage = "{0} boş geçilmemelidir.")]
+        [MaxLength(30, ErrorMessage = "{0} {1} karekterden büyük olmamalıdır.")]
+        [MinLength(10, ErrorMessage = "{0} {1} karekterden küçük olmamalıdır.")]
+        [DataType(DataType.Password)]
+        [Compare("NewPassword", ErrorMessage = "Girmiş olduğunuz yeni şifreniz ile yeni şifrenizin tekrarı alanları birbiri ile uyumlu olmalıdır.")]
+        public string RepeatPassword { get; set; }
+    }
+}
