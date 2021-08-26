eb3bdf9b18a043e589f4fc2c7f0389716f0d6478
Atilla <63123956+atillakalay@users.noreply.github.com>
2021-08-26 17:07:49 +0300

    Role Assignment Action Created.

--- ProgremmesBlog.Mvc/Areas/Admin/Controllers/RoleController.cs
+++ ProgremmesBlog.Mvc/Areas/Admin/Controllers/RoleController.cs
@@ -1,21 +1,22 @@
-﻿using System.Linq;
-using System.Text.Json;
+﻿using System.Text.Json;
 using System.Threading.Tasks;
+using AutoMapper;
 using Entities.Concrete;
 using Entities.Dtos;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using ProgrammersBlog.Mvc.Helpers.Abstract;
 
 namespace ProgrammersBlog.Mvc.Areas.Admin.Controllers
 {
     [Area("Admin")]
-    public class RoleController : Controller
+    public class RoleController : BaseController
     {
         private readonly RoleManager<Role> _roleManager;
 
-        public RoleController(RoleManager<Role> roleManager)
+        public RoleController(RoleManager<Role> roleManager, UserManager<User> userManager, IMapper mapper, IImageHelper imageHelper) : base(userManager, mapper, imageHelper)
         {
             _roleManager = roleManager;
         }
@@ -42,5 +43,31 @@ namespace ProgrammersBlog.Mvc.Areas.Admin.Controllers
             });
             return Json(roleListDto);
         }
+
+        [HttpGet]
+        [Authorize(Roles = "SuperAdmin,User.Update")]
+        public async Task<IActionResult> Assign(int userId)
+        {
+            var user = await UserManager.Users.SingleOrDefaultAsync(u => u.Id == userId);
+            var roles = await _roleManager.Roles.ToListAsync();
+            var userRoles = await UserManager.GetRolesAsync(user);
+            UserRoleAssignDto userRoleAssignDto = new UserRoleAssignDto
+            {
+                UserId = user.Id,
+                UserName = user.UserName
+            };
+            foreach (var role in roles)
+            {
+                RoleAssignDto roleAssignDto = new RoleAssignDto
+                {
+                    RoleId = role.Id,
+                    RoleName = role.Name,
+                    HasRole = userRoles.Contains(role.Name)
+                };
+                userRoleAssignDto.RoleAssignDtos.Add(roleAssignDto);
+            }
+            return PartialView("RoleAssignPartial", userRoleAssignDto);
+        }
+
     }
 }
