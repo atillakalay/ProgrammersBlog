75b4bb2078c81922e77016599e653544feee8543
Atilla <63123956+atillakalay@users.noreply.github.com>
2021-09-11 13:44:44 +0300

    12 New Articles Added to Our Database with New Migration.

--- DataAccess/Migrations/20210911103656_SeedingArticles.Designer.cs
+++ DataAccess/Migrations/20210911103656_SeedingArticles.Designer.cs
@@ -4,14 +4,16 @@ using DataAccess.Concrete.EntityFramework.Contexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DataAccess.Migrations
 {
     [DbContext(typeof(ProgrammersBlogContext))]
-    partial class ProgrammersBlogContextModelSnapshot : ModelSnapshot
+    [Migration("20210911103656_SeedingArticles")]
+    partial class SeedingArticles
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
