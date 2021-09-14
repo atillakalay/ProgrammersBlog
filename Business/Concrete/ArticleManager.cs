c4fb6f0a20ebf7963d14ec59cc35681de2aedd1f
Atilla <63123956+atillakalay@users.noreply.github.com>
2021-09-14 09:54:04 +0300

    Sorting Function Added to Homepage.

--- Business/Concrete/ArticleManager.cs
+++ Business/Concrete/ArticleManager.cs
@@ -146,6 +146,7 @@ namespace Business.Concrete
 
         public async Task<IDataResult<ArticleListDto>> GetAllByPagingAsync(int? categoryId, int currentPage = 1, int pageSize = 5, bool isAscending = false)
         {
+            pageSize = pageSize > 20 ? 20 : pageSize;
             var articles = categoryId == null
                 ? await UnitOfWork.Articles.GetAllAsync(a => a.IsActive && !a.IsDeleted, a => a.Category, a => a.User)
                 : await UnitOfWork.Articles.GetAllAsync(a => a.CategoryId == categoryId && a.IsActive && !a.IsDeleted,
@@ -155,10 +156,11 @@ namespace Business.Concrete
             return new DataResult<ArticleListDto>(ResultStatus.Success, new ArticleListDto
             {
                 Articles = sortedArticles,
-                CategoryId = categoryId==null?null:categoryId.Value,
+                CategoryId = categoryId == null ? null : categoryId.Value,
                 CurrentPage = currentPage,
                 PageSize = pageSize,
-                TotalCount = articles.Count
+                TotalCount = articles.Count,
+                IsAscending = isAscending
             });
         }
 
--- ProgremmesBlog.Mvc/Controllers/HomeController.cs
+++ ProgremmesBlog.Mvc/Controllers/HomeController.cs
@@ -14,11 +14,11 @@ namespace ProgrammersBlog.Mvc.Controllers
         }
 
         [HttpGet]
-        public async Task<IActionResult> Index(int? categoryId, int currentPage = 1, int pageSize = 5)
+        public async Task<IActionResult> Index(int? categoryId, int currentPage = 1, int pageSize = 5, bool isAscending = false)
         {
             var articlesResult = await (categoryId == null
-                ? _articleService.GetAllByPagingAsync(null, currentPage, pageSize)
-                : _articleService.GetAllByPagingAsync(categoryId.Value, currentPage, pageSize));
+                ? _articleService.GetAllByPagingAsync(null, currentPage, pageSize, isAscending)
+                : _articleService.GetAllByPagingAsync(categoryId.Value, currentPage, pageSize, isAscending));
             return View(articlesResult.Data);
         }
     }
