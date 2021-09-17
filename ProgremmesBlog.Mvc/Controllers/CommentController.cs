e7a56f8ea78bd2c6ab07c3ceda30d1c564a9be57
Atilla <63123956+atillakalay@users.noreply.github.com>
2021-09-17 08:59:18 +0300

    Backend Parts of Comment Addition Processes Have Been Created.

---
+++ ProgremmesBlog.Mvc/Controllers/CommentController.cs
@@ -0,0 +1,50 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using Business.Abstract;
+using Core.Utilities.Results.ComplexTypes;
+using Entities.Dtos;
+using ProgrammersBlog.Mvc.Models;
+using ProgrammersBlog.Shared.Utilities.Extensions;
+
+namespace ProgrammersBlog.Mvc.Controllers
+{
+    public class CommentController : Controller
+    {
+        private readonly ICommentService _commentService;
+
+        public CommentController(ICommentService commentService)
+        {
+            _commentService = commentService;
+        }
+
+        [HttpPost]
+        public async Task<JsonResult> Add(CommentAddDto commentAddDto)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _commentService.AddAsync(commentAddDto);
+                if (result.ResultStatus == ResultStatus.Success)
+                {
+                    var commentAddAjaxViewModel = JsonSerializer.Serialize(new CommentAddAjaxViewModel
+                    {
+                        CommentDto = result.Data,
+                        CommentAddPartial = await this.RenderViewToStringAsync("_CommentAddPartial", commentAddDto)
+                    }, new JsonSerializerOptions
+                    {
+                        ReferenceHandler = ReferenceHandler.Preserve
+                    });
+                    return Json(commentAddAjaxViewModel);
+                }
+                ModelState.AddModelError("", result.Message);
+            }
+            var commentAddAjaxErrorModel = JsonSerializer.Serialize(new CommentAddAjaxViewModel
+            {
+                CommentAddDto = commentAddDto,
+                CommentAddPartial = await this.RenderViewToStringAsync("_CommentAddPartial", commentAddDto)
+            });
+            return Json(commentAddAjaxErrorModel);
+        }
+    }
+}
---
+++ ProgremmesBlog.Mvc/Models/CommentAddAjaxViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Entities.Dtos;
+
+namespace ProgrammersBlog.Mvc.Models
+{
+    public class CommentAddAjaxViewModel
+    {
+        public CommentAddDto CommentAddDto { get; set; }
+        public string CommentAddPartial { get; set; }
+        public CommentDto CommentDto { get; set; }
+    }
+}
