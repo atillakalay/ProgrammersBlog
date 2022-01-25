dace96a81695d95762cc5a60f00f28a42353b26e
Atilla <63123956+atillakalay@users.noreply.github.com>
2022-01-25 13:06:45 +0300

    About Action and View Created with OptionsController.

--- Entities/Concrete/AboutUsPageInfo.cs
+++ Entities/Concrete/AboutUsPageInfo.cs
@@ -1,12 +1,39 @@
 ﻿
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
 namespace Entities.Concrete
 {
     public class AboutUsPageInfo
     {
+        [DisplayName("Başlık")]
+        [Required(ErrorMessage = "{0} alanı boş geçilmemelidir.")]
+        [MaxLength(150, ErrorMessage = "{0} alanı {1} karekterden büyük olmamalıdır.")]
+        [MinLength(5, ErrorMessage = "{0} alanı {1} karekterden küçük olmamalıdır.")]
         public string Header { get; set; }
+
+        [DisplayName("İçerik")]
+        [Required(ErrorMessage = "{0} alanı boş geçilmemelidir.")]
+        [MaxLength(1500, ErrorMessage = "{0} alanı {1} karekterden büyük olmamalıdır.")]
+        [MinLength(5, ErrorMessage = "{0} alanı {1} karekterden küçük olmamalıdır.")]
         public string Content { get; set; }
+
+        [DisplayName("Açıklama")]
+        [Required(ErrorMessage = "{0} alanı boş geçilmemelidir.")]
+        [MaxLength(100, ErrorMessage = "{0} alanı {1} karekterden büyük olmamalıdır.")]
+        [MinLength(5, ErrorMessage = "{0} alanı {1} karekterden küçük olmamalıdır.")]
         public string SeoDescription { get; set; }
+
+        [DisplayName("Seo Etiketleri")]
+        [Required(ErrorMessage = "{0} alanı boş geçilmemelidir.")]
+        [MaxLength(100, ErrorMessage = "{0} alanı {1} karekterden büyük olmamalıdır.")]
+        [MinLength(5, ErrorMessage = "{0} alanı {1} karekterden küçük olmamalıdır.")]
         public string SeoTags { get; set; }
+
+        [DisplayName("Seo Yazar")]
+        [Required(ErrorMessage = "{0} alanı boş geçilmemelidir.")]
+        [MaxLength(60, ErrorMessage = "{0} alanı {1} karekterden büyük olmamalıdır.")]
+        [MinLength(5, ErrorMessage = "{0} alanı {1} karekterden küçük olmamalıdır.")]
         public string SeoAuthor { get; set; }
     }
 }
