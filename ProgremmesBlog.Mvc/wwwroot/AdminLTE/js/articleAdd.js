54edc5b68bc4b601dce9b24e241eef5888687ffe
Atilla <63123956+atillakalay@users.noreply.github.com>
2022-01-25 21:56:59 +0300

    Completed Frontend Operations of ArticleRightSideBarWidgetOptions Class.

--- ProgremmesBlog.Mvc/wwwroot/AdminLTE/js/articleAdd.js
+++ ProgremmesBlog.Mvc/wwwroot/AdminLTE/js/articleAdd.js
@@ -1,28 +1,6 @@
 ﻿$(document).ready(function () {
 
-    //Trumbowyg
 
-    $('#text-editor').trumbowyg({
-        btns: [
-            ['viewHTML'],
-            ['undo', 'redo'], // Only supported in Blink browsers
-            ['formatting'],
-            ['strong', 'em', 'del'],
-            ['superscript', 'subscript'],
-            ['link'],
-            ['insertImage'],
-            ['justifyLeft', 'justifyCenter', 'justifyRight', 'justifyFull'],
-            ['unorderedList', 'orderedList'],
-            ['horizontalRule'],
-            ['removeformat'],
-            ['fullscreen'],
-            ['foreColor', 'backColor'],
-            ['emoji'],
-            ['fontfamily']
-        ]
-    });
-
-    //Trumbowyg
 
     //Select2
 
@@ -32,12 +10,30 @@
         allowClear: true
     });
 
+    $('#filterByList').select2({
+        theme: 'bootstrap4',
+        placeholder: "Lütfen bir filtre türü seçiniz...",
+        allowClear: true
+    });
+
+    $('#orderByByList').select2({
+        theme: 'bootstrap4',
+        placeholder: "Lütfen bir filtre türü seçiniz...",
+        allowClear: true
+    });
+
+    $('#isAscendingList').select2({
+        theme: 'bootstrap4',
+        placeholder: "Lütfen bir filtre tipi seçiniz...",
+        allowClear: true
+    });
+
     //Select2
 
     //JQuery UI -DatePicker
 
     $(function () {
-        $("#datepicker").datepicker({
+        $("#startAtDatePicker").datepicker({
             closeText: "kapat",
             prevText: "&#x3C;geri",
             nextText: "ileri&#x3e",
@@ -58,11 +54,38 @@
             duration: 1000,
             showAnim: "drop",
             showOptions:{direction:"down"},
-            minDate: -3,
-            maxDate: +3,
+          /*  minDate: -3,*/
+            maxDate: 0,
+
+        });
+
+        $("#endAtDatePicker").datepicker({
+            closeText: "kapat",
+            prevText: "&#x3C;geri",
+            nextText: "ileri&#x3e",
+            currentText: "bugün",
+            monthNames: ["Ocak", "Şubat", "Mart", "Nisan", "Mayıs", "Haziran",
+                "Temmuz", "Ağustos", "Eylül", "Ekim", "Kasım", "Aralık"],
+            monthNamesShort: ["Oca", "Şub", "Mar", "Nis", "May", "Haz",
+                "Tem", "Ağu", "Eyl", "Eki", "Kas", "Ara"],
+            dayNames: ["Pazar", "Pazartesi", "Salı", "Çarşamba", "Perşembe", "Cuma", "Cumartesi"],
+            dayNamesShort: ["Pz", "Pt", "Sa", "Ça", "Pe", "Cu", "Ct"],
+            dayNamesMin: ["Pz", "Pt", "Sa", "Ça", "Pe", "Cu", "Ct"],
+            weekHeader: "Hf",
+            dateFormat: "dd.mm.yy",
+            firstDay: 1,
+            isRTL: false,
+            showMonthAfterYear: false,
+            yearSuffix: "",
+            duration: 1000,
+            showAnim: "drop",
+            showOptions: { direction: "down" },
+            /*  minDate: -3,*/
+            maxDate: 0,
 
         });
     });
+
 
     //JQuery UI -DatePicker
 
---
+++ ProgremmesBlog.Mvc/wwwroot/AdminLTE/js/articleRightSideBarWidgetSettings.js
@@ -0,0 +1,69 @@
+﻿$(document).ready(function () {
+
+    //Trumbowyg
+
+    $('#text-editor').trumbowyg({
+        btns: [
+            ['viewHTML'],
+            ['undo', 'redo'], // Only supported in Blink browsers
+            ['formatting'],
+            ['strong', 'em', 'del'],
+            ['superscript', 'subscript'],
+            ['link'],
+            ['insertImage'],
+            ['justifyLeft', 'justifyCenter', 'justifyRight', 'justifyFull'],
+            ['unorderedList', 'orderedList'],
+            ['horizontalRule'],
+            ['removeformat'],
+            ['fullscreen'],
+            ['foreColor', 'backColor'],
+            ['emoji'],
+            ['fontfamily']
+        ]
+    });
+
+    //Trumbowyg
+
+    //Select2
+
+    $('#categoryList').select2({
+        theme: 'bootstrap4',
+        placeholder: "Lütfen bir kategori seçiniz...",
+        allowClear: true
+    });
+
+    //Select2
+
+    //JQuery UI -DatePicker
+
+    $(function () {
+        $("#datepicker").datepicker({
+            closeText: "kapat",
+            prevText: "&#x3C;geri",
+            nextText: "ileri&#x3e",
+            currentText: "bugün",
+            monthNames: ["Ocak", "Şubat", "Mart", "Nisan", "Mayıs", "Haziran",
+                "Temmuz", "Ağustos", "Eylül", "Ekim", "Kasım", "Aralık"],
+            monthNamesShort: ["Oca", "Şub", "Mar", "Nis", "May", "Haz",
+                "Tem", "Ağu", "Eyl", "Eki", "Kas", "Ara"],
+            dayNames: ["Pazar", "Pazartesi", "Salı", "Çarşamba", "Perşembe", "Cuma", "Cumartesi"],
+            dayNamesShort: ["Pz", "Pt", "Sa", "Ça", "Pe", "Cu", "Ct"],
+            dayNamesMin: ["Pz", "Pt", "Sa", "Ça", "Pe", "Cu", "Ct"],
+            weekHeader: "Hf",
+            dateFormat: "dd.mm.yy",
+            firstDay: 1,
+            isRTL: false,
+            showMonthAfterYear: false,
+            yearSuffix: "",
+            duration: 1000,
+            showAnim: "drop",
+            showOptions:{direction:"down"},
+            minDate: -3,
+            maxDate: +3,
+
+        });
+    });
+
+    //JQuery UI -DatePicker
+
+});
