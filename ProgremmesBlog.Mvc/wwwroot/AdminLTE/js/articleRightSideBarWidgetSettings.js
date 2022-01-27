302ee551bd8e66043e1e751f759dc318490d42aa
Atilla <63123956+atillakalay@users.noreply.github.com>
2022-01-27 09:39:45 +0300

    Implemented Dynamic Min Date Setting for jQuery UI DatePicker.

--- ProgremmesBlog.Mvc/wwwroot/AdminLTE/js/articleRightSideBarWidgetSettings.js
+++ ProgremmesBlog.Mvc/wwwroot/AdminLTE/js/articleRightSideBarWidgetSettings.js
@@ -1,43 +1,38 @@
 ﻿$(document).ready(function () {
 
-    //Trumbowyg
 
-    $('#text-editor').trumbowyg({
-        btns: [
-            ['viewHTML'],
-            ['undo', 'redo'], // Only supported in Blink browsers
-            ['formatting'],
-            ['strong', 'em', 'del'],
-            ['superscript', 'subscript'],
-            ['link'],
-            ['insertImage'],
-            ['justifyLeft', 'justifyCenter', 'justifyRight', 'justifyFull'],
-            ['unorderedList', 'orderedList'],
-            ['horizontalRule'],
-            ['removeformat'],
-            ['fullscreen'],
-            ['foreColor', 'backColor'],
-            ['emoji'],
-            ['fontfamily']
-        ]
-    });
-
-    //Trumbowyg
-
-    //Select2
-
+    // Select2
     $('#categoryList').select2({
         theme: 'bootstrap4',
         placeholder: "Lütfen bir kategori seçiniz...",
         allowClear: true
     });
 
-    //Select2
+    $('#filterByList').select2({
+        theme: 'bootstrap4',
+        placeholder: "Lütfen bir filtre türü seçiniz...",
+        allowClear: true
+    });
 
-    //JQuery UI -DatePicker
+    $('#orderByList').select2({
+        theme: 'bootstrap4',
+        placeholder: "Lütfen bir sıralama türü seçiniz...",
+        allowClear: true
+    });
+
+    $('#isAscendingList').select2({
+        theme: 'bootstrap4',
+        placeholder: "Lütfen bir sıralama tipi seçiniz...",
+        allowClear: true
+    });
+
+    // Select2
+
+
+    // jQuery UI - DatePicker
 
     $(function () {
-        $("#datepicker").datepicker({
+        $("#startAtDatePicker").datepicker({
             closeText: "kapat",
             prevText: "&#x3C;geri",
             nextText: "ileri&#x3e",
@@ -57,13 +52,39 @@
             yearSuffix: "",
             duration: 1000,
             showAnim: "drop",
-            showOptions:{direction:"down"},
-            minDate: -3,
-            maxDate: +3,
-
+            showOptions: { direction: "down" },
+            /*minDate: -3,*/
+            maxDate: 0,
+            onSelect: function (selectedDate) {
+                $("#endAtDatePicker").datepicker('option', 'minDate', selectedDate || getTodaysDate());
+            }
+        });
+        $("#endAtDatePicker").datepicker({
+            closeText: "kapat",
+            prevText: "&#x3C;geri",
+            nextText: "ileri&#x3e",
+            currentText: "bugün",
+            monthNames: ["Ocak", "Şubat", "Mart", "Nisan", "Mayıs", "Haziran",
+                "Temmuz", "Ağustos", "Eylül", "Ekim", "Kasım", "Aralık"],
+            monthNamesShort: ["Oca", "Şub", "Mar", "Nis", "May", "Haz",
+                "Tem", "Ağu", "Eyl", "Eki", "Kas", "Ara"],
+            dayNames: ["Pazar", "Pazartesi", "Salı", "Çarşamba", "Perşembe", "Cuma", "Cumartesi"],
+            dayNamesShort: ["Pz", "Pt", "Sa", "Ça", "Pe", "Cu", "Ct"],
+            dayNamesMin: ["Pz", "Pt", "Sa", "Ça", "Pe", "Cu", "Ct"],
+            weekHeader: "Hf",
+            dateFormat: "dd.mm.yy",
+            firstDay: 1,
+            isRTL: false,
+            showMonthAfterYear: false,
+            yearSuffix: "",
+            duration: 1000,
+            showAnim: "drop",
+            showOptions: { direction: "down" },
+            /*minDate: -3,*/
+            maxDate: 0
         });
     });
 
-    //JQuery UI -DatePicker
 
+    // jQuery UI - DatePicker
 });
--- ProgremmesBlog.Mvc/wwwroot/AdminLTE/js/site.js
+++ ProgremmesBlog.Mvc/wwwroot/AdminLTE/js/site.js
@@ -44,3 +44,7 @@ function getJsonNetObjectById(parentObj, id) {
     }
     return null;
 }
+function getTodaysDate() {
+    let today = new Date();
+    return `${today.getDate()}/${today.getMonth() + 1}/${today.getFullYear()}`;
+}
